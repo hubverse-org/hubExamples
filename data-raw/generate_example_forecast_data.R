dae9d1864ddf722b09c6f01d7001fd451bd78f27
Becky Sweger <rsweger@umass.edu>
2024-08-26 17:37:15 -0400

    Refactor forecast data script into functions
    Putting the code that generates the example datasets into their own
    functions will make it easier to test changes.

--- data-raw/generate_example_forecast_data.R
+++ data-raw/generate_example_forecast_data.R
@@ -11,30 +11,45 @@ if (isFALSE(check_bucket)) {{ stop("Aborting: ", s3_bucket_name, " S3 bucket not
 
 hub_path <- s3_bucket(s3_bucket_name)
 
-l_keep <- c("25", "48")
-q_lvls_keep <- c("0.05", "0.1", "0.25", "0.5", "0.75", "0.9", "0.95")
-d_keep <- c("2022-11-19", "2022-12-17")
-forecast_outputs <- hubData::connect_hub(hub_path, file_format = "parquet", skip_checks = TRUE) |>
-  dplyr::filter(
-    location %in% l_keep,
-    output_type != "quantile" |
-      (output_type == "quantile" & output_type_id %in% q_lvls_keep),
-    reference_date %in% d_keep
-  ) |>
-  hubData::collect_hub()
+create_forecast_outputs <- function() {
+  l_keep <- c("25", "48")
+  q_lvls_keep <- c("0.05", "0.1", "0.25", "0.5", "0.75", "0.9", "0.95")
+  d_keep <- c("2022-11-19", "2022-12-17")
+  forecast_outputs <- hubData::connect_hub(hub_path, file_format = "parquet", skip_checks = TRUE) |>
+    dplyr::filter(
+      location %in% l_keep,
+      output_type != "quantile" |
+        (output_type == "quantile" & output_type_id %in% q_lvls_keep),
+      reference_date %in% d_keep
+    ) |>
+    hubData::collect_hub()
+}
 
-target_ts_data_path <- paste("target-data", "time-series.csv", sep = "/")
-forecast_target_ts <- read_csv(
-  get_object(target_ts_data_path, bucket = s3_bucket_name),
-  show_col_types = FALSE
-)
+create_forecast_target_ts <- function() {
+  target_ts_data_path <- paste("target-data", "time-series.csv", sep = "/")
+  forecast_target_ts <- read_csv(
+    get_object(target_ts_data_path, bucket = s3_bucket_name),
+    show_col_types = FALSE
+  )
+}
 
-target_observations_data_path <- paste("target-data", "target-observations.csv", sep = "/")
-forecast_target_observations <- read_csv(
-  get_object(target_observations_data_path, bucket = s3_bucket_name),
-  show_col_types = FALSE
-)
+create_forecast_target_observations <- function() {
+  target_observations_data_path <- paste("target-data", "target-observations.csv", sep = "/")
+  forecast_target_observations <- read_csv(
+    get_object(target_observations_data_path, bucket = s3_bucket_name),
+    show_col_types = FALSE
+  )
+}
 
-usethis::use_data(forecast_outputs, overwrite = TRUE)
-usethis::use_data(forecast_target_ts, overwrite = TRUE)
-usethis::use_data(forecast_target_observations, overwrite = TRUE)
+# Allow this script to be sourced without running the functions
+# https://stackoverflow.com/a/47178017
+if (sys.nframe() == 0) {
+  forecast_outputs <- create_forecast_outputs()
+  usethis::use_data(forecast_outputs, overwrite = TRUE)
+
+  forecast_target_ts <- create_forecast_target_ts()
+  usethis::use_data(forecast_target_ts, overwrite = TRUE)
+
+  forecast_target_observations <- create_forecast_target_observations()
+  usethis::use_data(forecast_target_observations, overwrite = TRUE)
+}
--- tests/testthat/test-generate_example_forecast_data.R
+++ tests/testthat/test-generate_example_forecast_data.R
@@ -1,3 +1,4 @@
 test_that("forecast_outputs dataset is generated correctly", {
+  load(test_path("testdata", "forecast_outputs.rda"))
   expect_equal("1", "1")
 })
